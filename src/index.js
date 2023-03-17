9ffecc51a36be111e5b1be8660b1520c5ebd9414
郑伟泽 <1753961676@qq.com>
2023-03-17 16:58:19 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// 获取脚本配置
+const { SHOW_CONSOLE } = hamibot.env;
+
+// 等待开启无障碍权限
+auto.waitFor();
+
+// 显示控制台
+if (SHOW_CONSOLE) {
+    console.show();
+    sleep(300);
+    // 修改控制台位置
+    console.setPosition(0, 100);
+    // 修改控制台大小
+    console.setSize(device.width, device.height / 4);
+}
+
+// 在控制台输出 Hello Hamibot
+toastLog("Hello Hamibot");
+
+const _packageName = 'com.taobao.taobao';
+
+var taobao = app.getPackageInfo(_packageName);
+
+if (app.launch(_packageName)) {
+    toastLog("start taobao success.");
+}
